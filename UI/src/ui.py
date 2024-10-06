c2efdaf16259bc7cfeff031163e74b1ee5098328
pelapor_incident@protonmail.com <pelapor_incident@protonmail.com>
2024-10-06 22:24:46 +0700

    add check gas fee

---
+++ UI/src/ui.py
@@ -0,0 +1,51 @@
+
+from web3 import Web3
+
+# Ganti dengan URL RPC lo sendiri
+rpc_url = "https://rpc.sepolia.org"
+web3 = Web3(Web3.HTTPProvider(rpc_url))
+
+# Alamat smart contract lo di Sepolia
+contract_address = "0xYOUR_CONTRACT_ADDRESS"
+
+# ABI dari smart contract lo (copy dari file ABI Hardhat)
+contract_abi = [
+    {
+        "inputs": [],
+        "name": "getCurrentGasPriceWei",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getCurrentGasPriceETH",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+# Buat object contract
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+# Panggil fungsi untuk mendapatkan gas price dalam wei
+gas_price_wei = contract.functions.getCurrentGasPriceWei().call()
+print(f"Gas Price in Wei: {gas_price_wei}")
+
+# Panggil fungsi untuk mendapatkan gas price dalam ETH
+gas_price_eth = contract.functions.getCurrentGasPriceETH().call()
+print(f"Gas Price in ETH: {gas_price_eth}")
+
