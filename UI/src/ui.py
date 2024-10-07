f8919f63cd7d4a8cd529a58597f870c2e72b7c54
pelapor_incident@protonmail.com <pelapor_incident@protonmail.com>
2024-10-07 19:29:50 +0700

    initiate

--- UI/src/ui.py
+++ UI/src/ui.py
@@ -2,41 +2,41 @@
 from web3 import Web3
 
 # Ganti dengan URL RPC lo sendiri
-rpc_url = "https://rpc.sepolia.org"
+rpc_url = "http://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 
 # Alamat smart contract lo di Sepolia
-contract_address = "0xYOUR_CONTRACT_ADDRESS"
+contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 # ABI dari smart contract lo (copy dari file ABI Hardhat)
 contract_abi = [
     {
-        "inputs": [],
-        "name": "getCurrentGasPriceWei",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "getCurrentGasPriceETH",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     },
     {
-        "inputs": [],
-        "name": "getCurrentGasPriceETH",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "getCurrentGasPriceWei",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     }
-]
+  ]
 
 # Buat object contract
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
